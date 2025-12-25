3d191504cd4251d8ca600a3f085cb9da82d626bf
amigii <amiglezima@gmail.com>
2025-12-25 17:59:16 -0600

    Definí las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-
+import { Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
 import './App.css';
 
 
@@ -6,7 +10,15 @@ function App() {
 
   return (
     <>
-      <h1 >Ylink Dev</h1>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/register' element={<RegisterPage/>} />
+          <Route path='/dashboard' element={<DashboardPage/>} />
+        </Routes>
+      </div>
+
     </>
   )
 }
