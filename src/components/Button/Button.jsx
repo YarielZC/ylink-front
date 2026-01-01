f5216cac3a12775fccacca7bdf3bd761447bfdb0
JustEMAKILLER <emanuelnuviola@gmail.com>
2026-01-01 16:05:38 -0500

    Correción en componente Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import './Button.css'
 export default function Button({ children, onClick, type, variant, className, styles }) {
   return (
-    <button onClick={onClick} type={type} className={`btn-${variant} ${className}`} styles={styles}>
+    <button onClick={onClick} type={type} className={`btn-${variant} ${className}`} style={styles}>
       {children}
     </button>
   );
