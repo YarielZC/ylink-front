5c761329d2c83b630c24ffc0aaa891c4ffd16d72
JustEMAKILLER <emanuelnuviola@gmail.com>
2026-01-06 16:37:23 -0500

    implementación de props a Href: variant_color y have_decoration

--- src/components/Href/Href.jsx
+++ src/components/Href/Href.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
 import './Href.css'
 
-export default function Href({to, className, children}) {
+export default function Href({to, className, variant_color, have_decoration, children}) {
   return (
     <div className='Href'>
-      <Link to={to} className={className}>{children}</Link>
+      <Link to={to} className={`${className} ${variant_color} ${have_decoration ? 'decoration' : ''}`}>{children}</Link>
     </div>
   )
 }
