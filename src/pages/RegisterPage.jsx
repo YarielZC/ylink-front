c754d8c9d64b53cfeb85387b820866d2949fc810
amigii <amiglezima@gmail.com>
2026-01-10 14:53:55 -0600

    Agregar componente SignUp en RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,5 +1,10 @@
+import SignUp from "../components/SignUp/SignUp";
+
 function RegisterPage() {
+    <>
     return <h1>RegisterPage</h1>
+    <SignUp></SignUp>
+    </>
 }
 
 export default RegisterPage;
