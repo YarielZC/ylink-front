9672f0aa62a9dc9c785e337dbc76791de84a4e80
JustEMAKILLER <emanuelnuviola@gmail.com>
2026-01-12 20:38:35 -0500

    implementación de funcion login con fetching a api en AuthContext

--- src/context/Auth/AuthContext.jsx
+++ src/context/Auth/AuthContext.jsx
@@ -1,47 +1,67 @@
-import { AuthContext } from "./ExportAuthContext"
-import { useState, useEffect } from "react"
+import { AuthContext } from "./ExportAuthContext";
+import { useState, useEffect } from "react";
 
-export default function AuthProvider({children}) {
-  const [user, setUser] = useState(null)
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const [ token, setToken ] = useState('')
+export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [token, setToken] = useState("");
 
-  const login = ( userData, token ) =>{
-    setUser(userData)
-    setIsAuthenticated(true)
-    setToken(token)
+  const registerLoginData = (userData, token) => {
+    setUser(userData);
+    setIsAuthenticated(true);
+    setToken(token);
 
-    localStorage.setItem('token', token)
-    localStorage.setItem('userData', JSON.stringify(userData))
-  }
+    localStorage.setItem("token", token);
+    localStorage.setItem("userData", JSON.stringify(userData));
+  };
 
-  const logOut  = () =>{
-    setUser(null)
-    setIsAuthenticated(false)
-    setToken('')
-
-    localStorage.removeItem('token')
-    localStorage.removeItem('userData')
-  }
-
-  useEffect(()=>{
-    const localToken = localStorage.getItem('token')
-    const localUserData = localStorage.getItem('userData')
-
-    if(localToken && localUserData){
-      setToken(localToken)
-      setUser(JSON.parse(localUserData))
-      setIsAuthenticated(true)
-    }else {
-      setToken('')
-      setIsAuthenticated(false)
+  const login = async (username, password) => {
+    const user = username;
+    const pass = password;
+    if (!user || !pass) {
+      throw new Error("Credenciales inválidas");
     }
-  }, [] )
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) {
+        throw new Error("Failed to fetch");
+      }
+      const loginData = await response.json();
+      const { userData, token } = loginData;
+      login(userData, token);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  const logOut = () => {
+    setUser(null);
+    setIsAuthenticated(false);
+    setToken("");
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("userData");
+  };
+
+  useEffect(() => {
+    const localToken = localStorage.getItem("token");
+    const localUserData = localStorage.getItem("userData");
+
+    if (localToken && localUserData) {
+      setToken(localToken);
+      setUser(JSON.parse(localUserData));
+      setIsAuthenticated(true);
+    } else {
+      setToken("");
+      setIsAuthenticated(false);
+    }
+  }, []);
 
   return (
-    <AuthContext.Provider value={{user, isAuthenticated, token, login, logOut}}>
+    <AuthContext.Provider
+      value={{ user, isAuthenticated, token, registerLoginData, login, logOut }}
+    >
       {children}
     </AuthContext.Provider>
-  )
-
+  );
 }
