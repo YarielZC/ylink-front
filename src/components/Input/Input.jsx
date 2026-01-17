75712884a1782d1ac3aab9efe460016f74625122
lianni <liadev2006@gmail.com>
2026-01-16 20:28:44 -0500

    Corregir malas practicas de programacion y mejorar el estilo

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import { IconExclamationCircle, IconEye, IconEyeOff } from "@tabler/icons-react";
 import './Input.css'
 
-export default function Input ({ className, label, error, inputClassName, inputIcon, ref, type, ...inputProps }) {
+export default function Input({ className, label, error, inputClassName, inputIcon, ref, type, ...inputProps }) {
   const [showPass, setShowPass] = useState(false)
 
-  const handleShowPassClick = () =>{
+  const handleShowPassClick = () => {
     setShowPass(!showPass)
   }
 
@@ -15,24 +15,24 @@ export default function Input ({ className, label, error, inputClassName, inputI
       {label && (
         <label htmlFor={inputProps.name} className="input-label">
           {label}
-            </label>
+        </label>
       )}
 
       <div className="input-icon">
         {inputIcon}
       </div>
 
-      {type=='password' && 
-        <button onClick={handleShowPassClick} className="eyePass">
-          {showPass ? <IconEye/> : <IconEyeOff/>}
+      {type == 'password' &&
+        <button onClick={handleShowPassClick} type="button" className="eyePass">
+          {showPass ? <IconEye /> : <IconEyeOff />}
         </button>}
-            
+
       <input
-        ref={ref} 
-          id={inputProps.name}
-            className={`input-field ${type=='password' ? 'input-is-pass' : ''} ${inputIcon ? 'input-have-icon' : ''} ${inputClassName || ''} ${error ? 'input-error' : ''}`}
-            type={showPass ? 'text' : type}
-              {...inputProps} 
+        ref={ref}
+        id={inputProps.name}
+        className={`input-field ${type == 'password' ? 'input-is-pass' : ''} ${inputIcon ? 'input-have-icon' : ''} ${inputClassName || ''} ${error ? 'input-error' : ''}`}
+        type={showPass ? 'text' : type}
+        {...inputProps}
       />
 
       {error && (
@@ -41,7 +41,7 @@ export default function Input ({ className, label, error, inputClassName, inputI
             size={16}
             color="#ef4444"
           />
-          {error.message || error} 
+          {error.message || error}
         </span>
       )}
     </div>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,6 @@
+
 import React from "react";
+import './Login.css'
 import Card from "../Card/Card";
 import Input from '../Input/Input';
 import Button from '../Button/Button.jsx';
@@ -12,67 +14,57 @@ function Login() {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log("Logueando", data);
+    console.log('Logueando', data);
   };
 
   return (
-    <>
-      <h1>Welcome back</h1>
-      <p>Sign in to manage your links and analytics</p>
 
-      <Card>
+    <div className='login-container'>
+      <Card className='login-card'>
+
+        <form className='form' onSubmit={handleSubmit(onSubmit)}>
+
+          <Input className='login-input'
+            error={errors.email_username}
+            label={'Email or Username'}
+            type='text'
+            placeholder='Username or email'
+            {...register('email_username', { required: 'This field is required', minLength: { value: 4, message: 'Minimum 4 characters' } })}
+          />
+
+          <Input className='login-input'
+            error={errors.password}
+            label={'Password'}
+            type='password'
+            placeholder='. . . . . . . . . .'
+            {...register('password', {
+              required: 'This field is required',
+              minLength: {
+                value: 8,
+                message: 'Minimum 8 characters'
+              },
+              pattern: {
+                value: /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).+$/,
+                message: 'Must contain at least one uppercase letter, one number, and one special character'
+              },
+              validate: {
+                noSpaces: value => !/\s/.test(value) || 'Spaces are not allowed',
+              }
+            })}
+
+          />
 
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <label>Email or Username
-              <Input
-                type="text"
-                placeholder="Username or email"
-                {...register("emailOrUsername", { required: "Este campo es obligatorio" })}
-              />
-            </label>
-            {errors.emailOrUsername && <p style={{ color: "red" }}>{errors.emailOrUsername.message}</p>}
-          </div>
-
-
-          <label>Password
-            <Input
-              type="password"
-              placeholder=". . . . . . . ."
-              {...register("password", {
-                required: "Este campo es obligatorio",
-                minLength: {
-                  value: 8,
-                  message: "La contraseña debe tener al menos 8 caracteres"
-                },
-                pattern: {
-                  value: /^(?=.*[A-Z])(?=.*\d).+$/,
-                  message: "Debe contener al menos una mayúscula y un número"
-                },
-                validate: {
-                  noSpaces: value => !/\s/.test(value) || "No debe contener espacios",
-                  specialChar: value => /[!@#$%^&*(),.?":{}|<>]/.test(value) || "Debe contener al menos un carácter especial"
-                }
-              })}
-              onChange={(e) => {
-                // Puedes agregar lógica adicional aquí si necesitas
-                console.log("Contraseña cambiada:", e.target.value);
-              }}
-            />
-          </label>
-          {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
-
-          <Button type="submit">Sign in</Button>
+          <Button className='login-button' type='submit'>Sign in</Button>
         </form>
 
-        <p>Don’t have an account? Sign up for free</p>
-
+        <p className='login-signup-p'>Don’t have an account? <a href='/login'> Sign up for free </a></p>
 
       </Card>
-    </>
-  );
+    </div>
 
+  );
 
 }
 export default Login
+
